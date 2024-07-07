d7e358a1b0750e3536ebd6e570398af7cadb9f66
Rahul Singh <rsingh69749@gmail.com>
2024-07-08 02:21:44 +0530

    initial commit

--- Todo-list/src/assets/utils/DateUtil.js
+++ Todo-list/src/assets/utils/DateUtil.js
@@ -1,5 +1,9 @@
 const dateFormatter = new Intl.DateTimeFormat("en-IN", {
-  dateStyle: "medium",
+  hour12: true,
+  hour: "numeric",
+  minute: "numeric",
+  year: "numeric",
+  month: "short",
+  date: "2-digit"
 });
-
 export const formateDate = (date) => dateFormatter.format(date);
